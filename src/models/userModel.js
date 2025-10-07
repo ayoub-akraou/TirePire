83c900fc5b0857861eef7120221feedcb59eb02f
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-10-07 15:41:39 +0100

    feat: create user schema and model

---
+++ src/models/userModel.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			trim: true,
+			required: [true, "the name is required"],
+		},
+		slug: {
+			type: String,
+			lowercase: true,
+		},
+		email: {
+			type: String,
+			required: [true, "the email is required"],
+			unique: true,
+			lowercase: true,
+		},
+		phone: String,
+		profileImg: {
+			type: String,
+			default: "/images/profiles/profile-placeholder.jpeg",
+		},
+		password: {
+			type: String,
+			minLength: [8, "too short password"],
+			maxLength: [20, "too long password"],
+			required: [true, "the password is required"],
+		},
+		role: {
+			type: String,
+			enum: ["regular", "admin"],
+			default: "regular",
+		},
+	},
+	{ timestamps: true }
+);
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
