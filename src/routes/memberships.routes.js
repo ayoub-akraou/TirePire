901ed32956ca7d4fda81818b4b4acd9a6532d70d
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-10-10 17:15:12 +0100

    feat(routes): add memberships routes with CRUD operations and auth middleware

---
+++ src/routes/memberships.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import MembershipController from "../controllers/MembershipController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
+const router = Router();
+
+router.get("/", authMiddleware, MembershipController.index);
+router.post("/", authMiddleware, MembershipController.store);
+router.get("/:id", authMiddleware, MembershipController.show);
+// router.patch("/:id", authMiddleware, MembershipController.update);
+router.delete("/:id", authMiddleware, MembershipController.destroy);
+
+export default router;
