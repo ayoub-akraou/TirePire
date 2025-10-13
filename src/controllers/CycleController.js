00b1514a0755c0282abe12b152caae54982d31ff
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-10-13 12:17:54 +0100

    feat(CycleController): implement index method to retrieve cycles by group_id

--- src/controllers/CycleController.js
+++ src/controllers/CycleController.js
@@ -1,4 +1,14 @@
 import { param } from "express-validator";
 import CycleService from "../services/CycleService.js";
 
-export default class CycleController {}
+export default class CycleController {
+	static async index(req, res) {
+		try {
+			const { group_id } = req.params;
+			const cycles = await CycleService.getAll(group_id);
+			res.status(200).json({ success: true, data: cycles, message: "cycles retrieved successfully!" });
+		} catch (error) {
+			res.status(400).json({ success: false, message: error.message });
+		}
+	}
+}
