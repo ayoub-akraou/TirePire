f90cd735435eee8143df3b3831f1e25eed498964
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-11-10 19:36:43 +0100

    feat(routes): optimize the route name

--- src/routes/groups.routes.js
+++ src/routes/groups.routes.js
@@ -5,7 +5,7 @@ import authMiddleware from "../middlewares/authMiddleware.js";
 const router = Router();
 
 router.get("/", authMiddleware, GroupController.index);
-router.get("/createdBy/:userId", authMiddleware, GroupController.getGroupsCreatedByUser);
+router.get("/created", authMiddleware, GroupController.getGroupsCreatedByUser);
 router.post("/", authMiddleware, GroupController.store);
 router.get("/:id", authMiddleware, GroupController.show);
 // router.patch("/:id", authMiddleware, GroupController.update);
