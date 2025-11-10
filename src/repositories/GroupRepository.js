7a065c2c28b21112f8cef15f6e2a9fd1d6f08ffd
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-11-10 18:47:16 +0100

    feat: create GroupRepositotry

---
+++ src/repositories/GroupRepository.js
@@ -0,0 +1,21 @@
+import GroupModel from "../models/GroupModel.js";
+
+export default class GroupRepository {
+	static async getAll() {
+		return await GroupModel.find();
+	}
+
+	static async create(data) {
+		const newGroup = new GroupModel(data);
+		await newGroup.save();
+		return newGroup;
+	}
+
+	static async getOne(id) {
+		return await GroupModel.findById(id);
+	}
+
+    static async delete(id) {
+        return await GroupModel.findByIdAndDelete(id);
+    }
+}
