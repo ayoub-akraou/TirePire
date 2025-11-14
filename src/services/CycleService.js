9fef5d03d931c6f3e88d7cf5e3af5af01dc7c12c
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-11-14 15:15:06 +0100

    feat: replace model by repository calls

--- src/services/CycleService.js
+++ src/services/CycleService.js
@@ -1,7 +1,8 @@
-import GroupModel from "../models/GroupModel.js";
 import dayjs from "dayjs";
 import EmailService from "./EmailService.js";
 import GroupRepository from "../repositories/GroupRepository.js";
+import MembershipRepository from "../repositories/MembershipRepository.js";
+
 export default class CycleService {
 	static async getAll(group_id) {
 		const group = await GroupRepository.getOne({ _id: group_id });
@@ -13,44 +14,58 @@ export default class CycleService {
 		return group.cycles;
 	}
 
-	static async startCycle(group_id, start_date, user_id) {
+	static async startCycle(data) {
+		const { group_id, user_id, start_date } = data;
+		const cycle_order = data.cycle_order.map((user) => {
+			return {
+				member_id: user._id,
+				paymentByMember: data.cycle_order.map((member) => ({
+					member_id: member._id,
+					payed: false,
+				})).filter((member) => (String(member.member_id) !== String(user._id))),
+			};
+		});
 		const group = await GroupRepository.getOne({ _id: group_id });
+		if (!group) {
+			const error = new Error("group not found");
+			error.statusCode = 404;
+			throw error;
+		}
 
-		if (user_id !== group.admin_id) {
-			const error = new Error("the goup admin only can start the cycle!");
+		if (String(user_id) !== String(group.admin_id)) {
+			const error = new Error("the group admin only can create a cycle!");
 			error.statusCode = 403;
 			throw error;
 		}
 
-		const startDate = dayjs(start_date);
-		const cycleAlreadyStarted = Boolean(group.acceptMembers === false);
+		const nth = group.cycles.length + 1;
+		const numberOfMembers = cycle_order.length;
+		group.acceptMembers = false;
+		const cycle = {
+			cycle_number: nth,
+			start_date: dayjs(start_date).toDate(),
+			end_date: dayjs(start_date)
+				.add(group.frequency * numberOfMembers, "month")
+				.toDate(),
+			cycle_order,
+			currentTurn: 0,
+		};
+		group.cycles.push(cycle);
+		await group.save();
+		const updatedGroup = await GroupRepository.getOne({ _id: group._id });
+		const members = updatedGroup.cycles
+			.find((c) => c.cycle_number === nth)
+			.cycle_order.map((member) => member.member_id);
 
-		if (!cycleAlreadyStarted) {
-			group.acceptMembers = false;
-			const cycle = group.cycles.at(-1);
-			cycle.start_date = startDate;
-			const turn = cycle.currentTurn;
-			const turnMember = cycle.cycle_order[turn];
-
-			const members = cycle.cycle_order;
-			const numberOfMembers = cycle.cycle_order.length;
-			const endDate = startDate.add(group.frequency * numberOfMembers, "month");
-			cycle.end_date = endDate;
-			await group.save();
-			for (const m of members) {
-				await EmailService.send(
-					m.member_id.email,
-					"ayoubakraou@gmail.com",
-					`🔔 Le cycle ${group.name} démarre bientôt !`,
-					`	<p>Bonjour ${m.member_id.name},</p>
-					<p>Le cycle <b>${group.name}</b> commencera le <b>${startDate.format("DD/MM/YYYY")}</b>.</p>
+		for (const m of members) {
+			await EmailService.send(
+				m.email,
+				"ayoubakraou@gmail.com",
+				`🔔 Le cycle ${group.name} démarre bientôt !`,
+				`	<p>Bonjour ${m.member_id.name},</p>
+					<p>Le cycle <b>${group.name}</b> commencera le <b>${dayjs(start_date).format("DD/MM/YYYY")}</b>.</p>
 					<p>Montant à verser : <b>${group.amount} DH</b>.</p>`
-				);
-			}
-		} else {
-			const error = new Error("cycle already started");
-			error.statusCode = 409;
-			throw error;
+			);
 		}
 	}
 }
